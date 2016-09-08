6e82663e1b70b461b2976d547e4550964ac05c44
sean-a-m <ut.seanm@gmail.com>
2016-09-08 00:16:18 -0500

    Remove unused functions

--- src/GraphNamedThings/util.clj
+++ src/GraphNamedThings/util.clj
@@ -2,33 +2,20 @@
   (require [clojure.zip :as zip])
   (require digest))
 
-
-(defn in?
-  "Is an element in a collection?"
-  [elm coll]
-  (some #{elm} (into () coll)))
-
-(defn uuid! []
-  "Create a java uuid"
-  (str (java.util.UUID/randomUUID)))
-
-(defn abs
-  "Absolute value"
-  [n]
-  (max n (- n)))
-
-(defn true-or-fn
-  "Return value if comparison is true, otherwise return value of function
-  TODO: remember what this is called"
-  [value comp-fn false-fn]
-  (if (comp-fn value)
-    value
-    false-fn))
-
 (defn tails
   [xs]
   (lazy-seq
     (if (< 0 (count xs))
       (cons xs (tails (rest xs)))
       nil)))
+
+(defn uuid! []
+  "Create a java uuid"
+  (str (java.util.UUID/randomUUID)))
+
+(defn in?
+  "Is an element in a collection?"
+  [elm coll]
+  (some #{elm} (into () coll)))
+
 
