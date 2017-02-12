95df95f602e978d9fba2b54dc62ea972ae075fbc
sean-a-m <ut.seanm@gmail.com>
2017-02-12 09:45:44 -0600

    Move processing loop and add print statement

--- src/GraphNamedThings/core.clj
+++ src/GraphNamedThings/core.clj
@@ -1,4 +1,3 @@
-
 (ns GraphNamedThings.core
   (:require [GraphNamedThings.server :as server]
             [GraphNamedThings.processdata :as pc]
@@ -7,30 +6,23 @@
             [clj-time.coerce :as coerce])
   (:import [edu.stanford.nlp pipeline.StanfordCoreNLP pipeline.Annotation]))
 
-
-;(defn app [req]
-;  {:status 200
-;   :headers {"Content-Type" "text/html"}
-;   :body "hello HTTP!"})
-
 (defn update-response
   "Repeatedly update the document clusters returned by the server"
   [app-state]
-  (future
-    (loop []
-      (let [start-epoch (coerce/to-epoch
-                          (t/ago (t/hours 24)))
-            end-epoch (coerce/to-epoch
-                        (t/now))]
-        (webout/update-results app-state start-epoch end-epoch)
-        (Thread/sleep 60000))
-      (recur))))
+  (let [start-epoch (coerce/to-epoch
+                      (t/ago (t/hours 24)))
+        end-epoch (coerce/to-epoch
+                    (t/now))]
+    (doseq
+      (println "Updating results")
+      (webout/update-results app-state start-epoch end-epoch)
+      (Thread/sleep 60000))))
 
 (defn -main
   [& args]
   (let [props (doto (java.util.Properties.)
                 (.put "annotators" "tokenize, ssplit, pos, lemma, ner, parse, dcoref")
-                (.put "threads" 1)
+                (.put "threads" 2)
                 (.put "timeout" 30000))
         nlp-pipe (new StanfordCoreNLP props)
         app-state (atom nil)]
@@ -39,5 +31,10 @@
         (pc/process-things! nlp-pipe 1)
         (println "processing finished...")
         (recur)))
-    (update-response app-state)
+    (future
+      (loop []
+        (update-response app-state)
+        (recur)))
     (server/runserver app-state)))
+
+
