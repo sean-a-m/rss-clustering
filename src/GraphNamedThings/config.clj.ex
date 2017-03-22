abc0eeacd50c816d6b57a0ec7aa30c26f2a59f93
sean-a-m <ut.seanm@gmail.com>
2017-03-21 21:08:56 -0500

    move configuration file

--- src/GraphNamedThings/config.clj.ex
+++ src/GraphNamedThings/config.clj.ex
@@ -3,18 +3,18 @@
 
 
 "Path to database"
-(def output-db-path "//localhost:5432/adb")
-(def dbname "adb")
+(def output-db-path "//localhost:5432/db")
+(def dbname "db")
 (def dbhost "localhost:5432")
 
 "pg authentication"
-(def psql-user "")
-(def psql-pass "")
+(def psql-user "user")
+(def psql-pass "pass")
 
 ;Feed ids to use
 (def selected-feed-ids-ttrss [30 52 53 54 56 61 62 65 66 67 93 94 102])
 
-(def selected-feed-ids [3 4 7 9 10 11 12 13 14 15 16 17 18 97 98 99 100])
+(def selected-feed-ids  [3 4 7 9 13 14 15 16 17 18 97 98 99 100 104 105 106])
 
 ;version number used for determining when to overwrite database entries
 (def version 0.01)
