6170e2bb44a1e3c34c605775da760279ec10fc90
sean <sean.a.moles@gmail.com>
2017-10-14 21:33:57 -0500

    Remove unused code

--- src/GraphNamedThings/buildclusters.clj
+++ src/GraphNamedThings/buildclusters.clj
@@ -2,10 +2,6 @@
   (:require [GraphNamedThings.dbaccess :as dbio]
             [GraphNamedThings.louvain :as l]
             [GraphNamedThings.graphbuilder :as graphbuilder]
-            ;[clmcll.mcl :as mcl]
-            ;[louvainloom.louvain :as l]
-            [GraphNamedThings.feature-matrix :as fm]
-            [GraphNamedThings.kmeans :as kmeans]
             [GraphNamedThings.util :as util]
             [loom.graph :as graph]
             [medley.core :as me]
@@ -20,18 +16,6 @@
                                (me/distinct-by #(select-keys % [:title :content :scrape]))
                                (map :docid))]
         (filter #(util/in? (:docid %) distinct-ids) entity-records)))
-
-
-(defn kmeans-comms [start-epoch end-epoch]
-  (let [ent-recs (dbio/get-entity-records start-epoch end-epoch)
-        rev-doc-idx (zipmap (iterate inc 0)
-                            (distinct (map :docid ent-recs)))
-        K 40]
-    (let [cluster-results (->> ent-recs
-                               (fm/create-feature-matrix)
-                               (kmeans/find-centers K))]
-      (for [cluster (vals cluster-results)]
-        (map #(get rev-doc-idx %) cluster)))))
 
 (defn louvain-comms [start-epoch end-epoch]
   (let [records (dbio/get-entity-records start-epoch end-epoch)]
